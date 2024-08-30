7e62b97b7c3248d0a582cab83499ce7d6bc7379a
Josh Edson <joshedson@gmail.com>
2024-08-30 14:00:08 -0700

    Refactored some individual todo item content to its own MVVM - still didn't fix toggle issue but may have been required anyway.

---
+++ Firebase-Todo-Demo/View/ToDoItemDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ToDoItemDetailView.swift
+//  Firebase-Todo-Demo
+//
+//  Created by Josh Edson on 8/30/24.
+//
+
+import SwiftUI
+
+struct ToDoItemDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ToDoItemDetailView()
+}
---
+++ Firebase-Todo-Demo/ViewModel/ToDoItemViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ToDoItemViewModel.swift
+//  Firebase-Todo-Demo
+//
+//  Created by Josh Edson on 8/30/24.
+//
+
+import Foundation
